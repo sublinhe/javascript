56f932e1caefcb6245b08e15d950f54c559aa9af
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2025-01-01 19:15:03 -0300

    criei ex008 e 009

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20.5
+console.log(`A velocidade do do seu carro é ${vel}Km/hr`)
+if (vel > 100.2 ) { // Condição simples!
+    console.log('Você ultrapassou a velocidade permitida e foi MULTADO!')
+} 
+console.log(`dirija sempre usando cinto de segurança!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'nenhum'
+if (pais == 'Suiça') {
+    console.log('Você está na Suiça, um país altamente desenvolvido sem uma discrepante desigualdade social ')
+} else if (pais == 'Brasil'){
+    console.log(`Você está no ${pais}, um dos piores países do planeta terra. CORRA IMEDIATAMENTE!`)
+} else if (pais == 'nenhum'){
+    console.log('Você não está em país nenhum, se aloque em algum país para sua sobrevivência')
+}
