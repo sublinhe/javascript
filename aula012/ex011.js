8080fd92f94862828cce57698654988eb7cd0dee
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2025-01-01 22:40:09 -0300

    criei ex011

---
+++ aula012/ex011.js
@@ -0,0 +1,12 @@
+var idade = 100
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade >=67) 
+    {
+    console.log('Voto é absolutamente opcional')
+} 
+else {
+    console.log('Você é obrigado a votar, se não pagará multa')
+
+}
