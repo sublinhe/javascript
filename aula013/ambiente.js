4ffa667ef42ff709634d4cdf428e6245893fd8ef
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2025-01-17 00:56:29 -0300

    atualizei ambiente

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,13 +1,28 @@
-var c = 1
-while (c <= 6) {
-    console.log(`Passo ${c}`)
-    c++
-}
-
-var b = 5
+var b = 1
 
 do {
     console.log('Olá, Mundo!')
+    b++
+} while(b <= 6)
+
+
+
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
     c++
-} while(b >4);
+*/
+
+
+/*  
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
 
