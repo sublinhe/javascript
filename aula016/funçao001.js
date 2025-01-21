58452f9a66a99dceeba2e98be9a1478b15021b02
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2025-01-21 20:47:39 -0300

    terminei os exercícios da aula 16

---
+++ aula016/funçao001.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Deu Par!'
+    } else {
+        return 'impar'
+    }
+}
+
+console.log(parimpar(5))
---
+++ aula016/funçao002.js
@@ -0,0 +1,5 @@
+function soma (n1=0, n2=0, n3=0 ) {
+    return n1 + n2 - n3
+}
+
+console.log(soma(5, 2))
